95ac676353ac5ab9b4820b20fd784d3b81015ee8
eunsung <eunsung.shin@gmail.com>
2025-02-22 16:20:29 +0900

    pipeline input/output 형식 수정

--- pipeline/app.py
+++ pipeline/app.py
@@ -23,10 +23,9 @@ def pipeline(req: DetectionRequest) -> list[RecognitionResponseV2]:
     detection_response = requests.post(f'http://detection:8000/detection', json=req.model_dump()) 
     # recognition 처리는 저장된 파일로부터 수행
     detection_result = detection_response.json()
-    print('detection_result', detection_result)
     recognition_response = requests.post(f'http://recognition:8001/recognition', json=detection_result)
     recognition_result = recognition_response.json()    
-    print('recognition_result', recognition_result)
+    
     return recognition_result
 
 if __name__ == "__main__":
